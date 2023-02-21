4a4e8968bbe89fdce4668639901d1f61afc0b1ef
Pedro Fernandes <pedro.fernandes.moura@gmail.com>
2023-02-21 17:52:24 -0300

    adicionando data ao salvar
    quando salvar o arquivo ele não ira substituir o antigo e sim criar um novo com a data do dia

--- appy.py
+++ appy.py
@@ -3,48 +3,45 @@ import pandas as pd
 from docx import Document
 import tkinter as tk
 from tkinter import messagebox
+from datetime import datetime
 
-# Pedir ao usuário para inserir 4 cores e 4 nomes
-cores = [input(f"Insira a cor {i+1}: ") for i in range(4)]
-nomes = [input(f"Insira o nome {i+1}: ") for i in range(4)]
+# Pedir ao usuário para inserir o número de dados
+n = int(input("Insira o número de dados: "))
+colUm = input("Digite o nome da primeira coluna: ")
+colDois = input("Digite o nome da segunda coluna: ")
+
+# Pedir ao usuário para inserir as cores e nomes
+colUm = [input(f"Insira o dado referente {colUm} {i+1}: ") for i in range(n)]
+colDois = [
+    input(f"Insira o dado referente {colDois} {i+1}: ") for i in range(n)]
 
 # Criar um dataframe com as cores e nomes
-dados = {'Cor': cores, 'Nome': nomes}
+dados = {'Cor': colUm, 'Nome': colDois}
 df = pd.DataFrame(dados)
 
-# Define o caminho e nome do arquivo
-filepath_excel = 'C:\\Users\\x\\Documents\\Excel\\cores_e_nomes.xlsx'
-filepath_word = 'C:\\Users\\x\\Documents\\Word\\cores_e_nomes.docx'
-filepath_csv = 'C:\\Users\\x\\Documents\\csv\\cores_e_nomes.csv'
+# Define o caminho e nome do arquivo com a data do dia
+today = datetime.today().strftime('%Y-%m-%d')
+filepath_excel = f'C:\\Users\\x\\Documents\\Excel\\cores_e_nomes_{today}.xlsx'
+filepath_word = f'C:\\Users\\x\\Documents\\Word\\cores_e_nomes_{today}.docx'
+filepath_csv = f'C:\\Users\\x\\Documents\\csv\\cores_e_nomes_{today}.csv'
 
-if os.path.exists(filepath_excel):
-    root = tk.Tk()
-    root.withdraw()
-    result = messagebox.askyesno(
-        "Permitir salvar", "O arquivo " + filepath_excel + " já existe, deseja substituir?")
-    if result:
-        df.to_excel(filepath_excel, index=False)
-else:
+# Salva os arquivos sem substituir os existentes
+if not os.path.exists(filepath_excel):
     df.to_excel(filepath_excel, index=False)
-
-if os.path.exists(filepath_word):
-    root = tk.Tk()
-    root.withdraw()
-    result = messagebox.askyesno(
-        "Permitir salvar", "O arquivo " + filepath_word + " já existe, deseja substituir?")
-    if result:
-        document = Document()
-        table = document.add_table(rows=1, cols=2)
-        for i in range(len(df)):
-            cells = table.add_row().cells
-            cells[0].text = df.iloc[i]['Cor']
-            cells[1].text = df.iloc[i]['Nome']
-        document.save(filepath_word)
 else:
+    messagebox.showinfo(
+        "Arquivo existente", "O arquivo " + filepath_excel + " já existe.")
+
+if not os.path.exists(filepath_word):
     document = Document()
     table = document.add_table(rows=1, cols=2)
     for i in range(len(df)):
         cells = table.add_row().cells
         cells[0].text = df.iloc[i]['Cor']
         cells[1].text = df.iloc[i]['Nome']
-    document
+    document.save(filepath_word)
+else:
+    messagebox.showinfo(
+        "Arquivo existente", "O arquivo " + filepath_word + " já existe.")
+
+print("Salvo com sucesso!")
