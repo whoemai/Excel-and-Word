51d03f838c9154f82465454a584d7e95c802fc30
Pedro Fernandes <pedro.fernandes.moura@gmail.com>
2023-03-12 19:36:42 -0300

    quantidade de colunas
    -melhorando o codigo para saber quantas colunas o excel vai ter
    
    -validação de numero de colunas

--- teste.py
+++ teste.py
@@ -1,10 +1,4 @@
-import os
-import tkinter as tk
-from datetime import datetime
-from tkinter import messagebox
-
 import pandas as pd
-from docx import Document
 
 # Pedir ao usuário para inserir o número de dados
 n = int(input("Insira o número de dados: "))
@@ -33,19 +27,4 @@ for nome in nomesColunas:
 # Criar um dataframe com os dados
 df = pd.DataFrame(dados, columns=nomesColunas)
 
-# Define o caminho e nome do arquivo com a data do dia
-today = datetime.today().strftime('%Y-%m-%d')
-filepath_excel = f'C:\\Users\\x\\Documents\\Export Python\\appy-PY{today}.xlsx'
-
-# Salva os arquivos permitindo substituir os existentes
-if os.path.exists(filepath_excel):
-    confirm = messagebox.askyesno(
-        "Arquivo existente", f"O arquivo {filepath_excel} já existe. Deseja substituí-lo?")
-    if not confirm:
-        messagebox.showinfo("Arquivo não salvo", f"O arquivo {filepath_excel} não foi salvo.")
-    else:
-        df.to_excel(filepath_excel, index=False)
-else:
-    df.to_excel(filepath_excel, index=False)
-
-print("Excel salvo com sucesso!")
+print("sucesso")
